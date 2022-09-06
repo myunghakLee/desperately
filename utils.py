879b07452e9c486d95943c7ba1d60332386b98d8
myunghaklee <mh9716@kookmin.ac.kr>
2022-09-06 19:54:05 +0000

    Check hyper parameters

--- utils.py
+++ utils.py
@@ -75,21 +75,29 @@ def test(model, data, device, epoch = 0):
     return correct / all_data
 
 
-def normalize_max1(w):
+def normalize_max(w, mult = 0.4):
     for i in range(len(w)):
-        w[i] = w[i] / torch.max(abs(w[i]))
+        w[i] = w[i] / torch.max(abs(w[i])) * mult
     return w
 
 
 softmax = torch.nn.Softmax(dim=1)
 softmax2d = lambda b: softmax(torch.flatten(b, start_dim = 1)).reshape(b.shape)
-# f2 = lambda w, _=None: softmax2d(normalize_max1(-w)) * len(w[0])
-f2 = lambda w, _= None: softmax2d(-w) * len(w[0])
+f2 = lambda w, mult=None: softmax2d(normalize_max(-w, mult)) * len(w[0])
+# f2 = lambda w, mult = None: softmax2d(-w * mult) * len(w[0])
 
+# +
 to_gaussian = lambda arr, mean = 1, std = 1: ((arr - torch.mean(arr))/ (torch.std(arr) + 0.00001)) * std + mean
 
+def to_gaussian_rgb(img, mean, std): # rgb별로 따로 gaussian처리
 
-def get_LRP_img(img, label, model, criterion, optimizer, std = 0.1, mean = 1.0):
+    for i in range(3):
+        img[i] = ((img[i] - torch.mean(img[i])) / (torch.std(img[i]) + 0.00001)) * std[i] + mean[i]  # RGB
+    return img
+
+
+# +
+def get_LRP_img(img, label, model, criterion, optimizer, std = 0.1, mean = 1.0, mult = 500):
     img.requires_grad = True
     img.retain_grad = True
     
@@ -100,12 +108,34 @@ def get_LRP_img(img, label, model, criterion, optimizer, std = 0.1, mean = 1.0):
     optimizer.zero_grad()
 
     with torch.no_grad():
+        
+        gradient = (img*img.grad).clone()
+        gradient = f2(gradient, mult)
+#         img.grad *= 50
+#         img.grad += 1
+        
+        for i in range(len(gradient)):
+            gradient[i] = to_gaussian(gradient[i], mean = mean, std = std)
 
-        img_lrp = (img*img.grad).clone()
-        img_lrp = f2(img_lrp)
+#         for i, (m, s) in enumerate(zip(mean,std)):
+#             gradient[i] = to_gaussian_rgb(gradient[i], mean = m, std = s)
 
-        for i in range(len(img_lrp)):
-            img_lrp[i] = to_gaussian(img_lrp[i], std = std, mean = mean)
 
-        img_lrp = img*img_lrp # img_lrp가 음수값인것 지움
-    return img_lrp
+        mean, std = img.mean(dim=(2,3)), img.std(dim=(2,3))
+
+            
+        output_image = img * gradient
+        
+        for i, (m, s) in enumerate(zip(mean,std)):
+            output_image[i] = to_gaussian_rgb(output_image[i], mean = m, std = s)
+        
+#         print(torch.mean(img.grad))
+#         print("="*100)
+        
+#         img_lrp = f2(img_lrp)
+
+#         for i in range(len(img_lrp)):
+#             img_lrp[i] = to_gaussian(img_lrp[i], std = std, mean = mean)
+
+#         img_lrp = img*img_lrp # img_lrp가 음수값인것 지움
+    return output_image
